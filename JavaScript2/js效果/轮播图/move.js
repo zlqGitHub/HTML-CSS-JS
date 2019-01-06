a18ab7fdf1c9341fb12a9351abfb20299ddfa69e
zlqGitHub <1509874021@qq.com>
2019-01-06 10:46:53 +0800

    move函数+getStyle函数

--- JavaScript2/js效果/轮播图/move.js
+++ JavaScript2/js效果/轮播图/move.js
@@ -25,6 +25,47 @@ function move(obj,attr,target,speed,callback){
 	},30);
 }
 
+
+function move2(obj,attr,iTarget,fnEnd){
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function(){
+
+		var current = 0;
+		if(attr == "opacity")
+		{
+			current = Math.round(parseFloat(getStyle(obj,attr))*100);   //Math.round 四舍五入
+		}
+		else
+		{
+			current = parseInt(getStyle(obj,attr));
+		}
+
+		var speed = (iTarget - current)/6;
+		speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+
+		if(current == iTarget)
+		{
+			clearInterval(obj.timer);
+			if(fnEnd)fnEnd();
+		}
+		else
+		{
+			if(attr == "opacity")
+			{
+				obj.style.opacity = ( current + speed )/100;
+				obj.style.filter = "alpha(opacity:"+ (current + speed) +")";
+			}
+			else
+			{
+				obj.style[attr] = current + speed + "px";
+			}
+		}
+
+	},30);
+}	
+
+
+
 //获取任何元素的属性值
 function getStyle(obj,name){
 	//兼容性处理
