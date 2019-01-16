d37b43bd697eac6eb1f0cc218d280c3bf4512887
zlqGitHub <1509874021@qq.com>
2019-01-16 18:08:12 +0800

    js工具类

--- JavaScript2/js总结/tools.js
+++ JavaScript2/js总结/tools.js
@@ -175,3 +175,18 @@ function toggleClass(obj , cn){
 		addClass(obj , cn);
 	}
 }
+
+
+
+/*
+	JS中去除字符串两端的空格
+ */
+function trim(str){ //删除左右两端的空格
+　　return str.replace(/(^\s*)|(\s*$)/g, "");
+}
+function ltrim(str){ //删除左边的空格
+　　return str.replace(/(^\s*)/g,"");
+}
+function rtrim(str){ //删除右边的空格
+　　return str.replace(/(\s*$)/g,"");
+}
