551bf8422a1916da77c8bcb9c21b44cfcd46ac19
zlqGitHub <1509874021@qq.com>
2019-04-19 20:28:39 +0800

    使用export default 和 export 来暴露成员

---
+++ JS框架/vue框架/webpack/003/src/login.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>这是登录组件，使用 .vue 文件定义出来的 --- {{msg}}</h1>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){   //注意：组件中 data 必须是方法 function
+        return {
+            msg:"你好，我叫···",
+        }
+    },
+    methods:{
+        show(){
+            console.log("调用了login.vue中的show方法");
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
