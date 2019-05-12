7cd31bda054a22918d8c68d16081036036d0225e
zlqGitHub <1509874021@qq.com>
2019-05-12 15:10:58 +0800

    网页的逻辑文件

--- JS框架/Bootstrap/object/js/index.js
+++ JS框架/Bootstrap/object/js/index.js
@@ -1,4 +1,5 @@
 $(function () {
+    //jQuery可以多次、重复给元素绑定事件
     $(window).on("resize",function () {
         // console.log($(window).width());
         //获取页面中的所有item
@@ -30,4 +31,52 @@ $(function () {
     })
     //页面已加载就开始执行    使用trigger立即执行函数
     $(window).trigger("resize");
+
+    //动态改变产品特色区域的选项卡的滚动
+    $(window).on("resize",function () {
+        //获取ul及所有的li
+        let $ul = $("#product .nav");
+        //console.log($ul);
+        // let $allLis = $("[role=\"presentation\"]",$ul);
+        let $allLis = $("[role='presentation']",$ul);
+
+        //获取父元素及当前ul的宽度
+        let parentW = $ul.parent().width();
+        let ulW = 0;
+        $allLis.each(function (index,item) {
+            ulW += $(item).width();
+        });
+
+        if(ulW > parentW){
+            // $ul.css("width",ulW);
+            $ul.css({
+                "width": ulW + "px"
+            })
+        }
+        else{
+            $ul.removeAttr("style");
+        }
+    });
+
+    //点击头部选项卡显示出对应的内容
+    let allLis = $("#header .nav li");
+    let $hot = $("#hot");
+    //点击热门课程
+    $(allLis[2]).on("click",function () {
+        //console.log($hot.offset().top);
+        //通过动画效果来实现页面的滚动
+        $("html,body").animate({scrollTop:$("#hot").offset().top},1000)
+    })
+
+    //点击关于我们
+    let $about = $("#about");
+    $(allLis[0]).on("click",function () {
+        $("html,body").animate({"scrollTop":$about.offset().top},1000);
+    });
+
+
+    // 工具提示框初始化
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
 });
